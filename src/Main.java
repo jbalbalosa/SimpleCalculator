1c58dfd8324f888e944c0122055671eac7bb0921
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-02 10:58:40 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+             System.out.println("SimpleCalculator!");
+
+        SimpleCalculator calculator = new SimpleCalculator();
+//        calculator.setFirstNumber(5.0);
+//        calculator.setSecondNumber(4);
+//        System.out.println("add= " + calculator.getAdditionResult());
+//        System.out.println("subtract= " + calculator.getSubtractionResult());
+//        calculator.setFirstNumber(5.25);
+//        calculator.setSecondNumber(0);
+//        System.out.println("multiply= " + calculator.getMultiplicationResult());
+//        System.out.println("divide= " + calculator.getDivisionResult());
+
+        calculator.setFirstNumber(-1.25);
+        calculator.setSecondNumber(-1.25);
+        calculator.getDivisionResult();
     }
 }
